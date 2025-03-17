b8fff8128336f77acd21d3edb2cc5a868f10cf5d
Erjan Mukhamedjanov <tebafaz@gmail.com>
2025-03-17 20:34:26 +0500

    added error check in postgres

--- database/postgres.go
+++ database/postgres.go
@@ -18,6 +18,9 @@ func InitPostgres() error {
 	var err error
 	postgres, err = sqlx.Connect("postgres", fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		os.Getenv("POSTGRES_HOSTNAME"), os.Getenv("POSTGRES_USERNAME"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB_NAME"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_SSL_MODE")))
+	if err != nil {
+		return err
+	}
 	fmt.Printf("connection to %s created\n", postgres.DriverName())
 	return err
 }
